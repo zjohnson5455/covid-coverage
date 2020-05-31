50d7471893b1c31462890c79f42e1006baac93b9
Zack Johnson <zacharyjohnsonri@gmail.com>
2020-05-31 19:24:21 -0400

    added video class and a get related videos function

--- scrape.py
+++ scrape.py
@@ -1,7 +1,6 @@
-import argparse
-
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from video import Video
 
 f = open("key.txt", "r")
 keyPlusNewLine = f.readline()
@@ -11,45 +10,74 @@ f.close()
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
-SEARCH_TERM = 'basketball'
+SEARCH_TERM = 'coronavirus'
 
-def youtube_search():
-  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
 
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
-  search_response = youtube.search().list(
-    q=SEARCH_TERM,
-    part='id,snippet',
-    maxResults=10
-  ).execute()
+def get_related_videos(id):
+    search_response = youtube.search().list(
+        relatedToVideoId=id,
+        type='video',
+        part='id,snippet',
+        maxResults=5
+    ).execute()
 
-  videos = []
-  channels = []
-  playlists = []
+    neighbors = []
 
-  # Add each result to the appropriate list, and then display the lists of
-  # matching videos, channels, and playlists.
-  for search_result in search_response.get('items', []):
-    if search_result['id']['kind'] == 'youtube#video':
-      videos.append('%s (%s)' % (search_result['snippet']['title'],
-                                 search_result['id']['videoId']))
-    elif search_result['id']['kind'] == 'youtube#channel':
-      channels.append('%s (%s)' % (search_result['snippet']['title'],
-                                   search_result['id']['channelId']))
-    elif search_result['id']['kind'] == 'youtube#playlist':
-      playlists.append('%s (%s)' % (search_result['snippet']['title'],
-                                    search_result['id']['playlistId']))
+    # Add each result to the appropriate list, and then display the lists of
+    # matching videos, channels, and playlists.
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            id = search_result['id']['videoId']
+            title = search_result['snippet']['title']
+            video = Video(id, title)
+            neighbors.append(video)
+        else:
+            print('Found something that was not a video')
 
-  print 'Videos:\n', '\n'.join(videos), '\n'
-  print 'Channels:\n', '\n'.join(channels), '\n'
-  print 'Playlists:\n', '\n'.join(playlists), '\n'
 
+    for neighbor in neighbors:
+        print(neighbor.title)
+
+    return neighbors
+
+
+
+def youtube_search():
+
+    # Call the search.list method to retrieve results matching the specified
+    # query term.
+    search_response = youtube.search().list(
+        q=SEARCH_TERM,
+        type='video',
+        part='id,snippet',
+        maxResults=3
+    ).execute()
+
+    seedVideos = []
+
+    # Add each result to the appropriate list, and then display the lists of
+    # matching videos, channels, and playlists.
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            id = search_result['id']['videoId']
+            title = search_result['snippet']['title']
+            video = Video(id, title)
+            seedVideos.append(video)
+        else:
+            print('Found something that was not a video')
+
+
+    for vid in seedVideos:
+        print(vid.id)
+
+    return seedVideos
 
 if __name__ == '__main__':
 
-  try:
-    youtube_search()
-  except HttpError, e:
-    print 'An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)
+    try:
+        # youtube_search()
+        get_related_videos('BJXq83_GvY8')
+    except HttpError, e:
+        print 'An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)
---
+++ video.py
@@ -0,0 +1,4 @@
+class Video:
+    def __init__(self, id, title):
+        self.id = id
+        self.title = title
